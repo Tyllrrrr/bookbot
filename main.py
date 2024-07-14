1269d30510605dad84c4b0e3bc7b4cadf1b979ab
Tyllrrrr <TylerrrJP@gmail.com>
2024-07-13 22:18:15 -0500

    Added Number Count

--- main.py
+++ main.py
@@ -3,7 +3,25 @@ def main():
     text = book_text(book_path)
     print(text)
 
+    count = word_count(book_path)
+    if count != -1:
+        print(f"Number of words: {count}")
+
 def book_text(path):
     with open(path) as f:
         return f.read()
+    
+def word_count(book_path):
+    try:
+        with open(book_path, 'r') as file:
+            text = file.read()
+            words = text.split()
+            return len(words)
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found")
+        return ""
+    except IOError as e:
+        print(f"Error: Could not read file '{file_path}': {e}")
+        return ""
+
 main()
